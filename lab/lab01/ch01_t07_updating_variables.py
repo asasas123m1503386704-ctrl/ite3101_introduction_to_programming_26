65ef993e6ea7193b5f1927061cfdd29ebb507ead
HuangJunPeng，Alan <asasas123m1503386704@gmail.com>
2026-09-08 05:59:20 +0000

    [skip actions] lab/lab01/ch01_t07_updating_variables.py

--- lab/lab01/ch01_t07_updating_variables.py
+++ lab/lab01/ch01_t07_updating_variables.py
@@ -1,5 +1,6 @@
-january_to_june_rainfall=1.93
-july_rainfall=1.05
-august_rainfall=4.91
-september_rainfall=3.12
-total_rainfall=january_to_june_rainfall+july_rainfall+august_rainfall+ september_rainfall
+january_to_june_rainfall = 1.93
+july_rainfall = 1.05
+august_rainfall = 4.91
+september_rainfall = 3.12
+total_rainfall = january_to_june_rainfall + \
+    july_rainfall + august_rainfall + september_rainfall
